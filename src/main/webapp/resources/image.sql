5e3e423d714b208e741d99001a129b23bed3a7c3
방종윤 <freebjy@gmail.com>
2014-12-10 19:49:13 +0900

    12월 10월 sql문 추가

--- src/main/webapp/resources/image.sql
+++ src/main/webapp/resources/image.sql
@@ -1,13 +1,15 @@
-CREATE TABLE image (
-  img_fnum int(11) unsigned NOT NULL AUTO_INCREMENT,
-  img_id varchar(45) NOT NULL,
-  img_pname varchar(100) NOT NULL,
-  img_fsrc varchar(100) NOT NULL,
-  img_fname varchar(100) NOT NULL,
-  img_fsize bigint(20) unsigned NOT NULL,
-  PRIMARY KEY (img_fnum),
-  UNIQUE KEY img_fnum_UNIQUE (img_fnum),
-  KEY img_id_idx (img_id),
-  CONSTRAINT img_id_m_id_FK FOREIGN KEY (img_id) REFERENCES member (m_id) ON DELETE CASCADE 
-) ENGINE=InnoDB DEFAULT charset=utf8;
-
+CREATE TABLE `image` (
+  `img_num` int(11) NOT NULL AUTO_INCREMENT,
+  `img_m_id` varchar(45) NOT NULL,
+  `img_dir_num` int(11) NOT NULL,
+  `img_s_name` varchar(150) NOT NULL,
+  `img_o_name` varchar(150) NOT NULL,
+  `img_size` bigint(20) NOT NULL,
+  `img_thumb` mediumblob NOT NULL,
+  PRIMARY KEY (`img_num`),
+  UNIQUE KEY `img_num_UNIQUE` (`img_num`),
+  KEY `m_id_img_m_id_idx` (`img_m_id`),
+  KEY `dir_num_img_dir_num_idx` (`img_dir_num`),
+  CONSTRAINT `dir_num_img_dir_num` FOREIGN KEY (`img_dir_num`) REFERENCES `dir` (`dir_num`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `m_id_img_m_id` FOREIGN KEY (`img_m_id`) REFERENCES `member` (`m_id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
